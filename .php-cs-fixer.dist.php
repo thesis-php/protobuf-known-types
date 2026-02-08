b886fbcdf69c5e40b1b1be2fa5ca462bdb383c21
kafkiansky <vadimzanfir@gmail.com>
2026-02-08 10:15:58 +0300

    Generate google.protobuf known types

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+use PHPyh\CodingStandard\PhpCsFixerCodingStandard;
+
+$config = new Config()
+    ->setFinder(
+        Finder::create()
+            ->in(__DIR__ . '/src')
+            ->append([
+                __FILE__,
+            ]),
+    )
+    ->setParallelConfig(ParallelConfigFactory::detect())
+    ->setCacheFile(__DIR__ . '/var/' . basename(__FILE__) . '.cache');
+
+new PhpCsFixerCodingStandard()->applyTo($config, [
+    'fully_qualified_strict_types' => false,
+    'phpdoc_annotation_without_dot' => false,
+    'phpdoc_summary' => false,
+    'phpdoc_trim_consecutive_blank_line_separation' => false,
+]);
+
+return $config;
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Thesis Repository Template
+# Google protobuf known types.
 
-## Usage
+## Installation
 
-1. Create a new repository using this template.
-2. Clone it.
-3. Run `make` to scaffold the project.
+```shell
+composer require thesis/protobuf-known-types
+```
