bcf146e83959e4483cf599f7b59720ffbdf8bb43
Vadim Zanfir <vadimzanfir@gmail.com>
2026-04-01 21:17:15 +0300

    Use protoregistry (#3)

--- src/anypb.php
+++ src/anypb.php
@@ -6,7 +6,7 @@ namespace Google\Protobuf;
 
 use Thesis\Protobuf\Decoder;
 use Thesis\Protobuf\Encoder;
-use Thesis\Protobuf\Pool;
+use Thesis\Protobuf\Registry;
 
 const anyTypeUrlPrefix = 'type.googleapis.com/';
 
@@ -14,15 +14,17 @@ const anyTypeUrlPrefix = 'type.googleapis.com/';
  * @api
  * @template T of object
  * @param T $message
- * @param ?\Closure(T): ?non-empty-string $resolveClassType
+ * @param ?\Closure(T): non-empty-string $resolveClassType
  * @throws Encoder\EncodingError
  */
 function encodeAny(object $message, Encoder $encoder, ?\Closure $resolveClassType = null): Any
 {
-    $resolveClassType ??= static fn(object $_) => null;
+    $resolveClassType ??= static fn(object $message) => Registry\Pool::get()->classType($fqcn = $message::class) ?? throw new \RuntimeException(
+        "Associated with class '{$fqcn}' metadata not found in the \\Thesis\\Protobuf\\Registry\\Pool. Perhaps you forgot to include autoload.metadata.php in composer.json or did not call the appropriate descriptor registrar to register types in the pool?",
+    );
 
     return new Any(
-        typeUrl: $resolveClassType($message) ?? anyTypeUrlPrefix . Pool\Registry::get()->classType($message::class),
+        typeUrl: anyTypeUrlPrefix . $resolveClassType($message),
         value: $encoder->encode($message),
     );
 }
@@ -30,12 +32,16 @@ function encodeAny(object $message, Encoder $encoder, ?\Closure $resolveClassTyp
 /**
  * @api
  * @template T of object
- * @param ?\Closure(non-empty-string): ?class-string<T> $resolveType
+ * @param ?\Closure(non-empty-string): class-string<T> $resolveType
  * @throws Decoder\DecodingError
  */
 function decodeAny(Any $any, Decoder $decoder, ?\Closure $resolveType = null): object
 {
-    $resolveType ??= static fn(string $_) => null;
+    $resolveType ??= static fn(string $typeUrl) => Registry\Pool::get()
+        ->messageDescriptorByType($typeUrl) // @phpstan-ignore argument.type
+        ->fqcn ?? throw new \RuntimeException(
+            "Type metadata '{$typeUrl}' not found in the \\Thesis\\Protobuf\\Registry\\Pool. Perhaps you forgot to include autoload.metadata.php in composer.json or did not call the appropriate descriptor registrar to register types in the pool?",
+        );
 
     $typeUrl = $any->typeUrl;
     if (str_starts_with($typeUrl, anyTypeUrlPrefix)) {
@@ -46,9 +52,5 @@ function decodeAny(Any $any, Decoder $decoder, ?\Closure $resolveType = null): o
         throw new \RuntimeException('Type url in "google.protobuf.Any" cannot be empty.');
     }
 
-    $type = $resolveType($typeUrl) ?? Pool\Registry::get()
-        ->messageDescriptorByType($typeUrl)
-        ->fqcn;
-
-    return $decoder->decode($any->value, $type);
+    return $decoder->decode($any->value, $resolveType($typeUrl));
 }
